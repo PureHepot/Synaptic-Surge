adf821b704134b3ae094e7e3a73942556e3f9dfe
PureHepot <desripure66@gmail.com>
2024-10-16 19:55:49 +0800

    up

---
+++ Assets/Scripts/Laser/Door.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : BaseLaserInstrument
+{
+    public override void OnLaserHit(LaserControl laser)
+    {
+        base.OnLaserHit(laser);
+    }
+
+    public override void PowerOff()
+    {
+        base.PowerOff();
+    }
+
+    public override void PowerOn()
+    {
+        base.PowerOn();
+    }
+
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+    }
+
+    protected override void OnFrame()
+    {
+        base.OnFrame();
+    }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+}
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GameData
+{
+    public bool isFirstPlay;
+    public int playerLevel;
+}
+
+// 存档系统类
+public class SaveSystem
+{
+    // 存档文件的路径
+    private static string saveFilePath = Application.persistentDataPath + "/savefile.json";
+
+    // 保存数据
+    public static void SaveGame(GameData data)
+    {
+        try
+        {
+            // 将数据序列化为JSON格式
+            string jsonData = JsonUtility.ToJson(data, true);
+            // 将JSON数据写入文件
+            File.WriteAllText(saveFilePath, jsonData);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError("保存游戏进度时出错: " + ex.Message);
+        }
+    }
+
+    // 读取数据
+    public static GameData LoadGame()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            try
+            {
+                // 从文件读取JSON数据
+                string jsonData = File.ReadAllText(saveFilePath);
+                // 将JSON数据反序列化为GameData对象
+                GameData loadedData = JsonUtility.FromJson<GameData>(jsonData);
+                return loadedData;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("读取游戏进度时出错: " + ex.Message);
+                return null;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("存档文件不存在，无法读取进度");
+            return null;
+        }
+    }
+
+    // 删除存档
+    public static void DeleteSave()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            File.Delete(saveFilePath);
+        }
+        else
+        {
+            Debug.LogWarning("存档文件不存在，无法删除");
+        }
+    }
+
+    // 检查是否存在存档
+    public static bool SaveExists()
+    {
+        return File.Exists(saveFilePath);
+    }
+}
