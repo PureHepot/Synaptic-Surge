8d3b68e6a838cc90791cfd5192107daf34636fef
Tomori.Koio <2677300438@qq.com>
2024-10-16 21:59:18 +0800

    关卡跳转和一些小素材

---
+++ Assets/Scripts/EixtGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EixtGame : MonoBehaviour
+{
+    public void QuitGame() {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+}
